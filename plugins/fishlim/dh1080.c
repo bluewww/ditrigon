8def8d986a5282a18f34ca77af5e9492c9df7a1d
bluew <bluewww@users.noreply.github.com>
2026-02-17 22:28:21 +0100

    plugins/fishlim: Fix deprecated interfaces

--- plugins/fishlim/dh1080.c
+++ plugins/fishlim/dh1080.c
@@ -115,9 +115,9 @@ dh1080_decode_b64 (const char *data, gsize *out_len)
 	while (str->len % 4 != 0)
 		g_string_append_c (str, '=');
 
-	ret = g_base64_decode_inplace (str->str, out_len);
-	g_string_free (str, FALSE);
-  	return ret;
+	ret = (guchar *) g_string_free_and_steal (str);
+	g_base64_decode_inplace ((char *) ret, out_len);
+	return ret;
 }
 
 static char *
