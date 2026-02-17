6dd643660ce193e2713df31dbf12c5e880e74773
bluew <bluewww@users.noreply.github.com>
2026-02-17 19:48:44 +0100

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,16 @@
+;; .dir-locals.el
+((c-mode . ((indent-tabs-mode . t)   ; indent_style = tab
+	    (tab-width . 4)          ; indent_size = 4
+	    (c-basic-offset . 4)))
+
+ (c++-mode . ((indent-tabs-mode . t)
+	      (tab-width . 4)
+	      (c-basic-offset . 4)))
+
+ (objc-mode . ((indent-tabs-mode . t)
+	       (tab-width . 4)
+	       (c-basic-offset . 4)))
+
+ (csharp-mode . ((indent-tabs-mode . nil) ; indent_style = space
+		 (tab-width . 4)          ; indent_size = 4
+		 (c-basic-offset . 4))))
