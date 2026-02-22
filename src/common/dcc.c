16a2d03831f16f8c68440a957487e837311674ac
bluew <bluewww@users.noreply.github.com>
2026-02-22 17:32:45 +0100

    common: Remove win32 def

--- src/common/dcc.c
+++ src/common/dcc.c
@@ -39,12 +39,7 @@
 #define WANTDNS
 #include "inet.h"
 
-#ifdef WIN32
-#include <windows.h>
-#include <io.h>
-#else
 #include <unistd.h>
-#endif
 
 #include "hexchat.h"
 #include "util.h"
@@ -57,11 +52,6 @@
 #include "text.h"
 #include "url.h"
 #include "hexchatc.h"
-
-/* Setting _FILE_OFFSET_BITS to 64 doesn't change lseek to use off64_t on Windows, so override lseek to the version that does */
-#if defined(WIN32) && (!defined(__MINGW32__) && !defined(__MINGW64__))
-	#define lseek _lseeki64
-#endif
 
 /* interval timer to detect timeouts */
 static int timeout_timer = 0;
@@ -859,23 +849,6 @@ dcc_did_connect (GIOChannel *source, GIOCondition condition, struct DCC *dcc)
 {
 	int er;
 	
-#ifdef WIN32
-	if (condition & G_IO_ERR)
-	{
-		int len;
-
-		/* find the last errno for this socket */
-		len = sizeof (er);
-		getsockopt (dcc->sok, SOL_SOCKET, SO_ERROR, (char *)&er, &len);
-		EMIT_SIGNAL (XP_TE_DCCCONFAIL, dcc->serv->front_session,
-						 dcctypes[dcc->type], dcc->nick, errorstring (er),
-						 NULL, 0);
-		dcc->dccstat = STAT_FAILED;
-		fe_dcc_update (dcc);
-		return FALSE;
-	}
-
-#else
 	struct sockaddr_in addr;
 
 	memset (&addr, 0, sizeof (addr));
@@ -897,7 +870,6 @@ dcc_did_connect (GIOChannel *source, GIOCondition condition, struct DCC *dcc)
 			return FALSE;
 		}
 	}
-#endif
 	
 	return TRUE;
 }
@@ -2561,18 +2533,7 @@ dcc_add_file (session *sess, char *file, guint64 size, int port, char *nick, gui
 		}
 		if (prefs.hex_dcc_save_nick)
 		{
-#ifdef WIN32
-			char *t = strlen (dcc->destfile) + dcc->destfile;
-			strcpy (t, nick);
-			while (*t)
-			{
-				if (*t == '\\' || *t == '|')
-					*t = '_';
-				t++;
-			}
-#else
 			strcat (dcc->destfile, nick);
-#endif
 			strcat (dcc->destfile, ".");
 		}
 		strcat (dcc->destfile, file);
